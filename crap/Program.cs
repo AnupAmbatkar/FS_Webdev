d58c6eaa5021e75d240a23baf00307b88207f135
AnupAmbatkar <anupprashantambatkar@gmail.com>
2022-04-22 23:08:23 +0530

    todo

---
+++ crap/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Test{
+    class TestClass{
+static void Main(string[] args)
+{
+
+int odd=0,even=0;
+int[] arr=new int[8];
+for(int i=0;i<8;i++){
+    arr[i]=Convert.ToInt16(Console.ReadLine());
+    if(arr[i]%2==0){
+        even+=arr[i];
+    }
+    else{
+
+        odd+=arr[i];
+    }
+  }
+  Console.WriteLine("Sum of Odd Number is: {0} \n Sum of Even Number is: {1}",odd,even);
+}
+}
+}
